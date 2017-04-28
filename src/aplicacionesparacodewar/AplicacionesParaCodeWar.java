b7b0d16a83d85412ba0bb841fc5dee6e087732aa
dam107 <DAM107@PRO107.inforjm.local>
2017-04-28 10:27:40 +0200

    commit inicial

---
+++ src/aplicacionesparacodewar/AplicacionesParaCodeWar.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacionesparacodewar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/**
+ *
+ * @author DAM107
+ */
+public class AplicacionesParaCodeWar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+    
+    public static String WhoIsNext(String[] names, int n){
+        
+    }
+}
